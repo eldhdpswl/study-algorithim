f14e4ca8c6e2d4282d4dad0262635e5cfb53eddb
dks <89627776+eldhdpswl@users.noreply.github.com>
2023-02-28 22:18:07 +0900

    Update ch12-10.py
    ch12-10 new update

--- etc/ch12-10.py
+++ etc/ch12-10.py
@@ -44,3 +44,62 @@ def solution(key, lock):
                     for j in range(m):
                         new_lock[x + i][y + j] -= key[i][j]
     return False
+
+#---------------------------------------------------------------
+
+def rotation(key):
+    n = len(key)  # 행
+    m = len(key[0])  # 열
+    result = [[0] * m for _ in range(n)]
+    for i in range(n):
+        for j in range(m):
+            result[j][n - i - 1] = key[i][j]
+    return result        
+
+def check(new_lock):
+    lock_length = len(new_lock) // 3
+    for i in range(lock_length, lock_length * 2):
+        for j in range(lock_length, lock_length * 2):
+            if new_lock[i][j] != 1:
+                return False
+    return True        
+
+
+def solution(key, lock):
+    n = len(lock)
+    m = len(key)
+    # 자물쇠 3배 늘리기
+    new_lock = [[0] * (n * 3) for _ in range(n*3)] 
+    
+    # 3배 늘린 자물쇠 가운데에 자물쇠 넣기
+    for i in range(n):
+        for j in range(n):
+            new_lock[i+n][j+n] = lock[i][j]
+
+    for rotate in range(4):
+        # key 90도 회전
+        key = rotation(key)
+        
+        # 3배 자물쇠를 일반 자물쇠 크기의 2배만큼 끼워맞추면서 찾기
+        for x in range(n*2):
+            for y in range(n*2):
+                
+                # new 자물쇠에 열쇠 끼워넣기
+                for i in range(m):
+                    for j in range(m):
+                        new_lock[x + i][y + j] += key[i][j]
+                
+                # 자물쇠에 열쇠가 정확히 맞는지 검사
+                if check(new_lock) == True:
+                    return True
+                
+                # new 자물쇠에서 열쇠 빼기
+                for i in range(m):
+                    for j in range(m):
+                        new_lock[x+i][y+j] -= key[i][j]
+    return False
+
+
+
+
+
