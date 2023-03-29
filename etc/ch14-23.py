8283257a36a00fd7ec4eadcf321e626d8e13326a
dks <89627776+eldhdpswl@users.noreply.github.com>
2023-03-29 22:49:44 +0900

    Update ch14-23.py

--- etc/ch14-23.py
+++ etc/ch14-23.py
@@ -19,3 +19,23 @@ students.sort(key=lambda x: (-int(x[1]), int(x[2]), -int(x[3]), x[0]))
 # 정렬된 학생 정보에서 이름만 출력
 for student in students:
     print(student[0])
+
+'''    
+# map 설명
+map을 list로 변형하지 않으면 print(b)로 결과를 확인할 수가 없는데 이는 map object가 range()와 비슷하게 필요할 때만 하나씩 꺼내보여주기 때문입니다.
+
+map object의 들어있는 값을 보려면 next()를 이용하면 됩니다.
+
+a = [1,2,3,4,5]
+ 
+b = map(lambda x:x*2, a)
+print(next(b))  # 2
+print(next(b))  # 4
+print(next(b))  # 6
+print(next(b))  # 8 
+print(next(b))  # 10
+print(next(b))  # StopIteration error
+
+# 참고 https://jsp-dev.tistory.com/132
+
+'''    
