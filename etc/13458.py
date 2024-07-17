598d60d609942ba43e34d34293b7eca5ccdb44d0
dks <89627776+eldhdpswl@users.noreply.github.com>
2024-07-17 19:11:55 +0900

    Create 13458.py

---
+++ etc/13458.py
@@ -0,0 +1,15 @@
+
+import sys
+input = sys.stdin.readline
+
+
+N = int(input())
+lst = list(map(int, input().split()))
+B, C = map(int, input().split())
+
+ans = N
+for n in lst:
+    if n - B > 0:
+        ans += (n-B + C-1) // C
+
+print(ans)
